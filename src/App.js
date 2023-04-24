b2c7f58f8f9e1707b8bc5717df721b14f83287b6
shankar405 <shankarmalviya067@gmail.com>
2023-04-24 20:38:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
+
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
+
+  const addRecord = () => {
+    setData([
+      ...data,
+      {
+        name: name,
+        email: email,
+      },
+    ]);
+    setName("");
+    setEmail("");
+  };
+  const removedata = (index) => {
+    let arr = data;
+    arr.splice(index, 1);
+    setData([...arr]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* form section */}
+      <div className="form">
+        <Stack direction="row" spacing={2}>
+          <TextField
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            id="outlined-basic"
+            label="name"
+            variant="outlined"
+          />
+          <TextField
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            id="outlined-basic"
+            label="email "
+            variant="outlined"
+          />
+          <Button onClick={addRecord} variant="contained" color="success">
+            <AddIcon />
+          </Button>
+        </Stack>
+      </div>
+      {/* data store */}
+      <div className="data">
+        <div className="dataval">
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Remove</h4>
+        </div>
+      </div>
+      {data.map((element, index) => {
+        return (
+          <div key={index} className="dataval">
+            <h4>{element.name}</h4>
+            <h4>{element.email}</h4>
+            <Stack>
+              <Button
+                onClick={() => removedata(index)}
+                variant="contained"
+                color="error"
+              >
+                <DeleteIcon />
+              </Button>
+            </Stack>
+          </div>
+        );
+      })}
     </div>
   );
 }
